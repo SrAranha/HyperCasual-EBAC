f922f7d6428dd8f43ec427a08ae1151986fba553
Danilo "Aranha <daniloprado7@gmail.com>
2023-02-21 16:38:27 -0300

    Adicionado obstáculos e linha de chegada.
    Adicionado também as telas de vitória ao chegar na linha de chegada, e de derrota ao bater em um obstáculo.

---
+++ Assets/Scripts/Player/PlayerCollision.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    [Header("Lose Game")]
+    public GameObject loseScreen;
+    public string obstacleTag;
+    [Header("Win Game")]
+    public GameObject winScreen;
+    public string finishLineTag;
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Colidiu com " + collision.gameObject.tag);
+        if (collision.gameObject.CompareTag(obstacleTag))
+        {
+            LoseGame();
+        }
+        if (collision.gameObject.CompareTag(finishLineTag))
+        {
+            WinGame();
+        }
+    }
+    private void StopTime()
+    {
+        Time.timeScale = 0;
+    }
+    private void LoseGame()
+    {
+        StopTime();
+        loseScreen.SetActive(true);
+    }
+    private void WinGame()
+    {
+        StopTime();
+        winScreen.SetActive(true);
+    }
+}
