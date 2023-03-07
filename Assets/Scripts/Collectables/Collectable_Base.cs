1eee7e526e3da372964db6ed05274340f908c956
Danilo "Aranha <daniloprado7@gmail.com>
2023-03-07 15:39:50 -0300

    Adicionado VFX aos PowerUps e Moedas.

--- Assets/Scripts/Collectables/Collectable_Base.cs
+++ Assets/Scripts/Collectables/Collectable_Base.cs
@@ -1,10 +1,22 @@
+using System.Collections;
 using UnityEngine;
 
 public class Collectable_Base : MonoBehaviour
 {
     [Header("Collectable Base")]
     public string playerTag = "Player";
+    public float timeToHide;
 
+    public ParticleSystem _particle;
+    public MeshRenderer _meshRenderer;
+    public Collider _collider;
+
+    private void OnValidate()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _collider = GetComponent<Collider>();
+        _particle = GetComponentInChildren<ParticleSystem>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag(playerTag))
@@ -14,6 +26,18 @@ public class Collectable_Base : MonoBehaviour
     }
     protected virtual void Collect()
     {
+        StartCoroutine(HideCollectable(timeToHide));
+        _particle.Play();
+    }
+    IEnumerator HideCollectable(float time)
+    {
+        DisableCollectable();
+        yield return new WaitForSeconds(time);
         gameObject.SetActive(false);
+    }
+    private void DisableCollectable()
+    {
+        _meshRenderer.enabled = false;
+        _collider.enabled = false;
     }
 }
